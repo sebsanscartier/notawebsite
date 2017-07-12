5a1d801bce85ed64314eacf5520b24180a7dcc3f
Sebastien Sanscartier <sebastiensanscartier@Sebastiens-MacBook-Pro-2.local>
2017-07-12 18:08:29 -0400

    initial push

---
+++ hellonode.js
@@ -0,0 +1,26 @@
+//Load HTTP module
+var http = require("http");
+
+//Create HTTP server and listen on port 8000 for requests
+http.createServer(function (request, response) {
+
+   // Set the response HTTP header with HTTP status and Content type
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body "Hello World"
+   response.end('Hello World\n');
+}).listen(8000);
+
+// Print URL for accessing server
+console.log('Server running at http://127.0.0.1:8000/')
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
+})
